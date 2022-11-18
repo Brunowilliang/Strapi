f8b5ef38abd8f7e0e74f97a1143bc58dbc503967
Bruno Garcia <brunowilliang@icloud.com>
2022-11-18 13:13:51 -0300

    alterando o email

--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,17 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    email: {
+      config: {
+        provider: 'sendgrid',
+        providerOptions: {
+          apiKey: env('SENDGRID_API_KEY'),
+        },
+        settings: {
+          defaultFrom: 'eu@brunowilliang.com',
+          defaultReplyTo: 'eu@brunowilliang.com',
+        },
+      },
+    },
   });
+
